382c4b7c16ef325c21b6ddfc763c5fe06ebc3aa9
Collins Johnson <collins1104@gmail.com>
2023-03-14 14:10:02 -0400

    Finished this problem! On to the next!

---
+++ multiply_function_problem.js
@@ -0,0 +1,7 @@
+function multiply(a, b) {
+  a * b
+  return a * b;
+}
+multiply()
+
+/* You basically needed to use the return statement to end the execution of the function and specify what values are being returned to the function caller. You then need to actually call the function in order for it to produce any results to the console.*/
