bb5c8949c4358c7af4bbf85e9eaae6be22c54602
Collins Johnson <collins1104@gmail.com>
2023-03-14 14:23:15 -0400

    Just needed to add a semi-colon to the function call at the end and wanted to post the link to the problem that I solved! Now I am actually finished here lol!

--- multiply_function_problem.js
+++ multiply_function_problem.js
@@ -2,6 +2,8 @@ function multiply(a, b) {
   a * b
   return a * b;
 }
-multiply()
+multiply();
 
 /* You basically needed to use the return statement to end the execution of the function and specify what values are being returned to the function caller. You then need to actually call the function in order for it to produce any results to the console.*/
+
+/* https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript */
