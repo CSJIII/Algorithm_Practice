4696810ba2d35e6c25cb4964a242664981346623
Collins Johnson <collins1104@gmail.com>
2023-03-15 22:30:50 -0400

    Finished this problem! On to the next one!

---
+++ opposite_number_problem.js
@@ -0,0 +1,7 @@
+function opposite(number) {
+    return (number * -1);
+  }
+  
+/* To solve this one, you have to get the function to output the opposite of all the integers and floating-point numbers in the sample tests. I found that the easiest way to do this is to simply multiply those numbers by -1 since that will flip those values to their negative versions the same way that those numbers multiplied by 1 will just output the positive versions of those values! */
+
+/* https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript */
