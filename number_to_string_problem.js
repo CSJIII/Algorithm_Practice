6fe459f57ef99a702cc22233ea3a9fe7d590d1e5
Collins Johnson <collins1104@gmail.com>
2023-03-18 22:11:25 -0400

    Finished this problem! On to the next one!

---
+++ number_to_string_problem.js
@@ -0,0 +1,8 @@
+function numberToString(num) {
+var num = num.toString();
+  return num;
+}
+
+/* I basically just used the tostring method to convert the number into a string and then returned the string! */
+
+/* https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript */
