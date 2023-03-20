96611bcc10abfce9949d0cc90dd6d3a8f0e553b6
Collins Johnson <collins1104@gmail.com>
2023-03-20 19:14:19 -0400

    Finished this problem! On to the next one!

---
+++ sentence_smash_problem.js
@@ -0,0 +1,7 @@
+function smash (words) {
+    return words.join(" ")
+ };
+
+/* I basically solved this problem by using the join() method to create a new string by combining all of the strings from our words parameter into one new string and passed a space in the method to space out the words and form a sentence and not just one giant jumbled string */
+
+/* https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript */
